fb94757101d1eacfc7776d6d826589e451126638
Angela Duran <duranangela19@gmail.com>
2018-05-09 21:42:34 -0600

    Add merge sort functionality

--- lib/deck.rb
+++ lib/deck.rb
@@ -14,8 +14,43 @@ class Deck
       if @cards[index].value.to_i > @cards[index+1].value.to_i
         @cards[index], @cards[index+1] = @cards[index+1], @cards[index]
       end
-    end 
+    end
     @cards
+  end
+
+  def merge_sort #this method is ridiculously long - fix
+    sorted_cards = []
+    if @cards.length == 1
+      sorted_cards = @cards
+    end
+    #split cards array
+    middle = @cards.length/2
+    left_cards = @cards.slice(0, middle)
+    right_cards = @cards.slice(middle, @cards.length-middle)
+    #compare cards
+    index_l = 0
+    index_r = 0
+    while index_l < left_cards.length && index_r < right_cards.length
+      l = left_cards[index_l].value.to_i
+      r = right_cards[index_r].value.to_i
+      if l <= r
+        sorted_cards << left_cards[index_l]
+        index_l += 1
+      else
+        sorted_cards << right_cards[index_r]
+        index_r += 1
+      end
+    end
+    #one card left in one of the arrays
+    while index_l < left_cards.length
+      sorted_cards << left_cards[index_l]
+      index_l += 1
+    end
+    while index_r < right_cards.length
+      sorted_cards << right_cards[index_r]
+      index_r += 1
+    end
+    sorted_cards
   end
 
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -49,4 +49,24 @@ class DeckTest < Minitest::Test
     assert_equal [card_2, card_1, card_3, card_5, card_6, card_4], deck.sort
   end
 
+  def test_it_merge_sorts
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("3", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal [card_2, card_1, card_3], deck.merge_sort
+  end
+
+  def test_it_merge_sorts_more_cards
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("3", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("8", "Spades")
+    card_5 = Card.new("6", "Hearts")
+    card_6 = Card.new("7", "Clubs")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5, card_6])
+    assert_equal [card_2, card_1, card_3, card_5, card_6, card_4], deck.sort
+  end
+
+
 end
